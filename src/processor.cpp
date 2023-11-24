2eaf99b66cb1196fcc173f258f2a50088425191c
Nikita Parhar <nikita.parhar@iesve.com>
2023-11-24 09:29:25 +0000

    updated

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,6 +1,7 @@
 #include "processor.h"
 #include "linux_parser.h"
-using std::vector, std::string;
+using std::vector;
+using std::string;
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
     std::vector<string> cpu_jiffies_vector = LinuxParser::CpuUtilization();
